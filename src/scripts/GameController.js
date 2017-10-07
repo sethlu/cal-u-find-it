93761bedaf575cbb7c18ac3f1921aae391bc12ff
Seth Dumaguin <benkydu@berkeley.edu>
2017-10-07 03:18:56 -0700

    Mapped locations

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -1,5 +1,6 @@
 
 import {Controller} from "./component/Controller";
+import {Location} from "./Location";
 import {Level} from "./Level";
 
 let GameController = Controller.createComponent("GameController");
@@ -35,6 +36,8 @@ GameController.defineMethod("init", function () {
 
   /** A list of levels */
   this.levels = [];
+
+  this.locMarkers = [];
 
 });
 
@@ -76,13 +79,28 @@ GameController.defineMethod("selectLevel", function (level) {
 
   // Unset the graphics for the current level
 
-
+  for (let i = 0; i < this.locMarkers.length; i++) {
+    this.locMarkers[i].remove();
+  }
+  this.locMarkers = [];
 
   // Switch to new level
   this.level = level;
 
   // Set the graphics for the new level
 
+  let game_map = L.map('map').setView([36.7783, -119.4179], 6);
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {attribution: 'Map data &copy; 2017 OpenStreetMap contributors', minZoom: '5', maxZoom: '7'}).addTo(game_map);
+
+
+  let {question, locations} = this.levels[this.level];
+
+  for (let i = 0; i < locations.length; i++) {
+    let marker = L.marker([locations[i].lat, locations[i].lon]);
+    this.locMarkers.push(marker);
+    marker.addTo(game_map);
+
+  }
 
 
 });
