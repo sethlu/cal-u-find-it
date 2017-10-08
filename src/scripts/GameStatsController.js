960395a8f3675e3d6d4bb652c63accfa5f4ac8ed
Zhuo Lu <sethlu@mintkit.net>
2017-10-07 19:29:34 -0700

    Add stats board entering transition

--- src/scripts/GameStatsController.js
+++ src/scripts/GameStatsController.js
@@ -2,6 +2,7 @@
 import {SingleModelController} from "./component/SingleModelController";
 import {clearChildNodes} from "./util";
 import {cloneTemplate} from "./component/Controller";
+import {Waterfall} from "./component/Waterfall";
 
 let GameStatsController = SingleModelController.createComponent("GameStatsController");
 
@@ -45,15 +46,26 @@ GameStatsController.defineMethod("updateView", function () {
   let levelStatsElement = this.view.querySelector(".levels-stats");
   clearChildNodes(levelStatsElement);
 
+  let waterfall = new Waterfall();
+
   if (this.gameStats) Object.entries(this.gameStats.levels).forEach(function (entry) {
     let [index, levelStats] = entry;
 
     let template = cloneTemplate("#template-game-level-stats");
+    let element = template.querySelector(".level-stats");
+
+    element.classList.add("hidden"); // Hide the element
 
     template.querySelector(".level-prompt").innerText = levelStats.level.question;
     template.querySelector(".level-location-name").innerText = levelStats.level.locations[0].location;
 
     levelStatsElement.appendChild(template);
+
+    waterfall = waterfall
+      .then(function () {
+        element.classList.remove("hidden"); // Show the element
+      }, 80);
+
   });
 
 });
